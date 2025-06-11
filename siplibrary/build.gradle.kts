446995e774e6d762fe6828afaaa6b30183eaa14a
eddyslarez <eddyslares@gmail.com>
2025-06-11 13:28:27 +0300

    second commit

--- siplibrary/build.gradle.kts
+++ siplibrary/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    id("maven-publish")
     id("kotlin-parcelize")
+    id("maven-publish")
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10" // O la versión de Kotlin que uses
 }
 
 android {
@@ -26,12 +27,12 @@ android {
             )
         }
     }
-    
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    
+
     kotlinOptions {
         jvmTarget = "17"
     }
@@ -39,7 +40,7 @@ android {
     buildFeatures {
         compose = true
     }
-    
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.4"
     }
@@ -50,34 +51,20 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.lifecycle:lifecycle-process:2.7.0")
-    
-    // Compose BOM
+
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    
-    // WebRTC
+
     implementation("com.shepeliev:webrtc-kmp:0.125.9")
-    
-    // WebSocket
     implementation("org.java-websocket:Java-WebSocket:1.5.4")
-    
-    // JSON
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
-    
-    // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    
-    // DateTime
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.1")
-    
-    // DataStore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
-    
-    // Bluetooth permissions for Android 12+
     implementation("androidx.bluetooth:bluetooth:1.0.0-alpha02")
-    
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -87,11 +74,34 @@ publishing {
     publications {
         register<MavenPublication>("release") {
             groupId = "com.github.eddyslarez"
-            artifactId = "sip-library-android"
+            artifactId = "sip-library"
             version = "1.0.0"
 
             afterEvaluate {
                 from(components["release"])
+            }
+
+            pom {
+                name.set("SIP Library for Android")
+                description.set("Librería para WebRTC y WebSocket en Android con Compose.")
+                url.set("https://github.com/eddyslarez/sip-library")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("eddyslarez")
+                        name.set("Eddys Larez")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/eddyslarez/sip-library.git")
+                    developerConnection.set("scm:git:ssh://github.com/eddyslarez/sip-library.git")
+                    url.set("https://github.com/eddyslarez/sip-library")
+                }
             }
         }
     }
