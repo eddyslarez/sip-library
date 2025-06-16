046099d7ae5191bd797823a636fe5b4d3a3291be
eddyslarez <eddyslares@gmail.com>
2025-06-16 15:09:35 +0300

    12 commit

--- siplibrary/src/main/java/com/eddyslarez/siplibrary/utils/CryptoUtils.kt
+++ siplibrary/src/main/java/com/eddyslarez/siplibrary/utils/CryptoUtils.kt
@@ -1,18 +1,13 @@
 package com.eddyslarez.siplibrary.utils
 
-import java.security.MessageDigest
+import okio.ByteString.Companion.encodeUtf8
 
-/**
- * Crypto utilities for SIP authentication
- * 
- * @author Eddys Larez
- */
 
 /**
  * Computes MD5 hash of a string input
  */
 fun md5(input: String): String {
-    val md = MessageDigest.getInstance("MD5")
-    val digest = md.digest(input.toByteArray())
-    return digest.joinToString("") { "%02x".format(it) }
+    // Using placeholder for actual MD5 implementation
+    // Real implementation would use platform-specific crypto libraries
+    return input.encodeUtf8().md5().hex()
 }
